a70365f9feaec4cbcb3ed778e6d3183f0acda89a
Trey Manuszak <trey17manuszak@yahoo.com>
2021-08-03 22:25:24 -0700

    OnCurve works

--- BitcoinFromScratch/Library.fs
+++ BitcoinFromScratch/Library.fs
@@ -9,11 +9,10 @@ module EllipticCurve =
         }
 
     type Point = 
-        {
-            curve: Curve
-            x: bigint
-            y: bigint
-        }
+        val curve: Curve
+        val x: bigint
+        val y: bigint
+        new(c, x0, y0) = { curve = c; x = x0; y = y0 }
 
     let HexStringToInt (str: string) =
         let rec Number (str: string) (index: int) (length: int) (b: bigint, d: bigint) = 
@@ -23,9 +22,9 @@ module EllipticCurve =
                     elif str.[index] >= 'A' && str.[index] <= 'F' then b * (bigint 16), d + (bigint ((int str.[index]) - 55)) * b  // Character is A-F
                     else  // There is a bug if you are in here
                         printfn $"Here! {index} {length} {b} {d}"
-                        (bigint -1, bigint -1)
+                        bigint -1, bigint -1
                 else 
-                    let (bresult,dresult) = Number str (index+1) length (b, d)  // Not reached end of string. Keep recursing down then calculate on the way up.
+                    let (bresult, dresult) = Number str (index + 1) length (b, d)  // Not reached end of string. Keep recursing down then calculate on the way up.
                     if str.[index] >= '0' && str.[index] <= '9' then bresult * (bigint 16), dresult + (bigint ((int str.[index]) - 48)) * bresult  // Character is 0-9
                     elif str.[index] >= 'A' && str.[index] <= 'F' then bresult * (bigint 16), dresult + (bigint ((int str.[index]) - 55)) * bresult  // Character is A-F
                     else  // There is a bug if you are in here
@@ -40,6 +39,10 @@ module EllipticCurve =
             a = HexStringToInt "0x0000000000000000000000000000000000000000000000000000000000000000"
             b = HexStringToInt "0x0000000000000000000000000000000000000000000000000000000000000007"
         }
+
+    let G = new Point(BitcoinCurve, HexStringToInt "0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", HexStringToInt "0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8")
+    let OnCurve = ((G.y)**2 - (G.x)**3 - (bigint 7)) % BitcoinCurve.p = (bigint 0)
+    printfn $"Is on curve {OnCurve}"
         
         
 
